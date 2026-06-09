e71211326e1570b29de63deef2cb135c07e0ebb1
Dynnova <makesethpos@gmail.com>
2026-06-09 10:05:42 +0700

    Remove Supabase config and add DATA_URL

--- js/config.js
+++ js/config.js
@@ -1,3 +1,1 @@
-// Supabase configuration
-const SUPABASE_URL = "https://swykzsmwrflqmkdrpxlh.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_bZ0Fwcy4Zc_EH440GswtSg_kHS0Z3f0";
+const DATA_URL = "./data.json";
